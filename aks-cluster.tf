ffa396c7436f4306e4d813b1dd7cf1efc6b60a37
Rick Jen <rijen@microsoft.com>
2021-11-13 14:01:24 -0800

    changed from random_pet to random_string

--- aks-cluster.tf
+++ aks-cluster.tf
@@ -1,11 +1,11 @@
-resource "random_pet" "prefix" {}
+resource "random_string" "prefix" {}
 
 provider "azurerm" {
   features {}
 }
 
 resource "azurerm_resource_group" "default" {
-  name     = "${random_pet.prefix.id}-rg"
+  name     = "${random_string.prefix.id}-rg"
   location = "West US 2"
 
   tags = {
@@ -14,10 +14,10 @@ resource "azurerm_resource_group" "default" {
 }
 
 resource "azurerm_kubernetes_cluster" "default" {
-  name                = "${random_pet.prefix.id}-aks"
+  name                = "${random_string.prefix.id}-aks"
   location            = azurerm_resource_group.default.location
   resource_group_name = azurerm_resource_group.default.name
-  dns_prefix          = "${random_pet.prefix.id}-k8s"
+  dns_prefix          = "${random_string.prefix.id}-k8s"
 
   default_node_pool {
     name            = "default"
