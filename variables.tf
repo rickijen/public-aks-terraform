8198a16831166b66d64f19579293aeebb296befd
Rick Jen <rijen@microsoft.com>
2021-11-14 17:46:40 -0800

    added variables

--- variables.tf
+++ variables.tf
@@ -5,3 +5,12 @@ variable "appId" {
 variable "password" {
   description = "Azure Kubernetes Service Cluster password"
 }
+
+# refer https://azure.microsoft.com/pricing/details/monitor/ for log analytics pricing 
+variable log_analytics_workspace_sku {
+    default = "PerGB2018"
+}
+
+variable "ssh_public_key" {
+  description = "ssh public key"
+}
