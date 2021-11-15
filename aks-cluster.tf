124ba9aa9321a46a6853c08962ec5e3e0687efb1
Rick Jen <rijen@microsoft.com>
2021-11-14 16:35:14 -0800

    Changed to CNI networking

--- aks-cluster.tf
+++ aks-cluster.tf
@@ -13,22 +13,59 @@ resource "azurerm_resource_group" "default" {
   }
 }
 
+resource "azurerm_virtual_network" "default" {
+  name                = "${random_pet.prefix}-network"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+  address_space       = ["10.2.0.0/16"]
+}
+
+resource "azurerm_subnet" "default" {
+  name                 = "${random_pet.prefix}-akssubnet"
+  virtual_network_name = azurerm_virtual_network.default.name
+  resource_group_name  = azurerm_resource_group.default.name
+  address_prefixes     = ["10.2.0.0/22"]
+}
+
 resource "azurerm_kubernetes_cluster" "default" {
   name                = "${random_pet.prefix.id}-aks"
   location            = azurerm_resource_group.default.location
   resource_group_name = azurerm_resource_group.default.name
   dns_prefix          = "${random_pet.prefix.id}-k8s"
 
+/*
   default_node_pool {
     name            = "default"
     node_count      = 2
     vm_size         = "Standard_D2_v2"
     os_disk_size_gb = 30
   }
+*/
+
+  default_node_pool {
+    name                = "default"
+    node_count          = 2
+    vm_size             = "Standard_D2_v2"
+    os_disk_size_gb     = 30
+    #type                = "VirtualMachineScaleSets"
+    availability_zones  = ["1", "2"]
+    enable_auto_scaling = true
+    min_count           = 2
+    max_count           = 4
+
+    # Required for advanced networking
+    vnet_subnet_id = azurerm_subnet.default.id
+  }
 
   service_principal {
     client_id     = var.appId
     client_secret = var.password
+  }
+
+  network_profile {
+    network_plugin     = "azure"
+    load_balancer_sku  = "standard"
+    network_policy     = "azure"
   }
 
   role_based_access_control {
