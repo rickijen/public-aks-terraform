31ccfdcdde6832a34e2b7a2e70f2b86740cbf3e9
Rick Jen <rijen@microsoft.com>
2021-11-14 17:37:03 -0800

    moved the LA section in the code

--- aks-cluster.tf
+++ aks-cluster.tf
@@ -27,6 +27,31 @@ resource "azurerm_subnet" "default" {
   address_prefixes     = ["10.2.0.0/22"]
 }
 
+resource "random_id" "log_analytics_workspace_name_suffix" {
+    byte_length = 8
+}
+
+resource "azurerm_log_analytics_workspace" "default" {
+    # The WorkSpace name has to be unique across the whole of azure, not just the current subscription/tenant.
+    name                = "${random_pet.prefix.id}-${random_id.log_analytics_workspace_name_suffix.dec}"
+    location            = azurerm_resource_group.default.location
+    resource_group_name = azurerm_resource_group.default.name
+    sku                 = var.log_analytics_workspace_sku
+}
+
+resource "azurerm_log_analytics_solution" "default" {
+    solution_name         = "ContainerInsights"
+    location              = azurerm_log_analytics_workspace.default.location
+    resource_group_name   = azurerm_resource_group.default.name
+    workspace_resource_id = azurerm_log_analytics_workspace.default.id
+    workspace_name        = azurerm_log_analytics_workspace.default.name
+
+    plan {
+        publisher = "Microsoft"
+        product   = "OMSGallery/ContainerInsights"
+    }
+}
+
 resource "azurerm_kubernetes_cluster" "default" {
   name                = "${random_pet.prefix.id}-aks"
   location            = azurerm_resource_group.default.location
@@ -40,15 +65,6 @@ resource "azurerm_kubernetes_cluster" "default" {
           key_data = var.ssh_public_key
       }
   }
-
-/*
-  default_node_pool {
-    name            = "default"
-    node_count      = 2
-    vm_size         = "Standard_D2_v2"
-    os_disk_size_gb = 30
-  }
-*/
 
   default_node_pool {
     name                = "default"
@@ -78,31 +94,6 @@ resource "azurerm_kubernetes_cluster" "default" {
 
   role_based_access_control {
     enabled = true
-  }
-
-  resource "random_id" "log_analytics_workspace_name_suffix" {
-      byte_length = 8
-  }
-
-  resource "azurerm_log_analytics_workspace" "default" {
-      # The WorkSpace name has to be unique across the whole of azure, not just the current subscription/tenant.
-      name                = "${random_pet.prefix.id}-${random_id.log_analytics_workspace_name_suffix.dec}"
-      location            = azurerm_resource_group.default.location
-      resource_group_name = azurerm_resource_group.default.name
-      sku                 = var.log_analytics_workspace_sku
-  }
-
-  resource "azurerm_log_analytics_solution" "default" {
-      solution_name         = "ContainerInsights"
-      location              = azurerm_log_analytics_workspace.default.location
-      resource_group_name   = azurerm_resource_group.default.name
-      workspace_resource_id = azurerm_log_analytics_workspace.default.id
-      workspace_name        = azurerm_log_analytics_workspace.default.name
-
-      plan {
-          publisher = "Microsoft"
-          product   = "OMSGallery/ContainerInsights"
-      }
   }
 
   addon_profile {
