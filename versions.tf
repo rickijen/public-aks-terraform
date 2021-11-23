7b95bf47de105735b022fc32ae95983eea774e84
Rick Jen <rijen@microsoft.com>
2021-11-22 21:09:09 -0800

    updated verion

--- versions.tf
+++ versions.tf
@@ -2,9 +2,9 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 2.86.0"
+      version = "2.66.0"
     }
   }
 
-  #required_version = ">=0.14"
+  required_version = ">= 0.14"
 }
