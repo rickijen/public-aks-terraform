7c1ed0cdc1eeb6426ed17fc0d27592d5b5a10c2b
Rick Jen <rijen@microsoft.com>
2021-11-22 20:56:08 -0800

    added required azurerm provider version

--- aks-cluster.tf
+++ aks-cluster.tf
@@ -1,5 +1,14 @@
 resource "random_pet" "prefix" {}
 
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=2.86.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 }
