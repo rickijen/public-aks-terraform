4e328657033ae3ee91e8e85e2552f3e16f3803a0
Rick Jen <rijen@microsoft.com>
2021-11-23 09:46:08 -0800

    updated variables file

--- variables.tf
+++ variables.tf
@@ -9,22 +9,28 @@ variable "password" {
 */
 
 variable "admin_group_obj_id" {
+  type        = string
   description = "AKS Admin group object ID"
 }
 
 # refer https://azure.microsoft.com/pricing/details/monitor/ for log analytics pricing 
-variable log_analytics_workspace_sku {
-    default = "PerGB2018"
+variable "log_analytics_workspace_sku" {
+  type        = string
+  default     = "PerGB2018"
 }
 
 variable "ssh_public_key" {
+  type        = string
   description = "ssh public key"
 }
 
 variable "azure_policy_k8s_initiative" {
+  type        = string
   description = "Kubernetes cluster pod security baseline standards for Linux-based workloads"
 }
 
 variable "windowspassword" {
+  type        = string
   description = "Windows admin password"
+  sensitive   = true
 }
